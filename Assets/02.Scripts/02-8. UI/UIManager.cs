7bd7e8fd9114229fa72eb5b1d24d64a1b1a2529c
AByeong <106604214+AByeong@users.noreply.github.com>
2025-04-03 15:30:08 +0900

    Add | Book and Paper Assets in ExternalAssets
    - 책, 종이에 해당하는 에셋을 추가하였습니당

--- Assets/02.Scripts/02-8. UI/UIManager.cs
+++ Assets/02.Scripts/02-8. UI/UIManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -11,15 +12,40 @@ public class UIManager : Singleton<UIManager>
     [SerializeField] 
     private Button _settingButton;
     
+    [SerializeField]
+    private Button _guideBook;
     
     protected override void Awake()
     {
         base.Awake();
     }
 
-   
-    
-    
-    
+    private void Start()
+    {
+        _settingButton.onClick.AddListener(() => ShowSetting());
+        _guideBook.onClick.AddListener(() => GuideBookShow());
+    }
 
+    void ShowSetting()
+    {
+        Debug.Log("Show setting");
+    }
+
+    void GuideBookShow()
+    {
+        Debug.Log("Show guidebook");
+    }
+
+    void GuideBookHide()
+    {
+        Debug.Log("Hide guidebook");
+    }
+
+    void NextDialogue()
+    {
+        Debug.Log("Show next dialogue");
+    }
+    
+    
+    
 }
