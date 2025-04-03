f096b009601521bda8281575185eb2dbb8e082d9
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-03 16:59:28 +0900

    Feat | 모험가 타입에 따른 AdventurerData 초기화 구현

--- Assets/02.Scripts/02-2. Adventurer/Adventurer.cs
+++ Assets/02.Scripts/02-2. Adventurer/Adventurer.cs
@@ -3,16 +3,66 @@ using UnityEngine;
 public class Adventurer : MonoBehaviour
 {
     [SerializeField]
-    private AdventurerSO _adventurerData;
-    public AdventurerSO AdventurerData { get => _adventurerData; set => _adventurerData = value; }
+    private AdventurerSO _adventurerSO;
+    private AdventurerData _adventurerData;
+    public AdventurerData AdventurerData { get => _adventurerData; set => _adventurerData = value; }
 
     private void Awake()
     {
-        
+        InitAdventurerData();
     }
-
-    private void Update()
+    private void InitAdventurerData()
     {
-        
+        if (_adventurerSO.AdventurerType == AdventurerType.Major)
+        {
+            MajorASO majorAdventurerSO = (MajorASO)_adventurerSO;
+            InitMajorTypeData(ref majorAdventurerSO);
+        }
+        else
+        {
+            MinorASO minorAdventurerSO = (MinorASO)_adventurerSO;
+            InitMinorTypeData(ref minorAdventurerSO);
+        }
     }
+    private void InitMajorTypeData(ref MajorASO majorAdventurerSO)
+    {
+        _adventurerData = new AdventurerData(
+            majorAdventurerSO.AdventurerType,
+            majorAdventurerSO.AdventurerName,
+            majorAdventurerSO.AdventurerClass,
+            majorAdventurerSO.AdventurerTitle,
+            majorAdventurerSO.AdventurerTier,
+            majorAdventurerSO.OriginalSTR,
+            majorAdventurerSO.OriginalMAG,
+            majorAdventurerSO.OriginalINS,
+            majorAdventurerSO.OriginalDEX,
+            AdventurerStateType.Idle
+        );
+    }
+    private void InitMinorTypeData(ref MinorASO minorAdventurerSO)
+    {
+        string adventurerName 
+            = minorAdventurerSO.AdventurerNameList[Random.Range(0, minorAdventurerSO.AdventurerNameList.Count)];
+        string adventurerClass
+            = minorAdventurerSO.AdventurerClassList[Random.Range(0, minorAdventurerSO.AdventurerClassList.Count)];
+        string adventurerTitle
+            = minorAdventurerSO.AdventurerTitleList[Random.Range(0, minorAdventurerSO.AdventurerTitleList.Count)];
+
+        AdventurerTierType adventurerTier
+            = (AdventurerTierType)Random.Range((int)AdventurerTierType.A, (int)AdventurerTierType.D + 1);
+
+        _adventurerData = new AdventurerData(
+            minorAdventurerSO.AdventurerType,
+            adventurerName,
+            adventurerClass,
+            adventurerTitle,
+            adventurerTier,
+            minorAdventurerSO.OriginalSTR,
+            minorAdventurerSO.OriginalMAG,
+            minorAdventurerSO.OriginalINS,
+            minorAdventurerSO.OriginalDEX,
+            AdventurerStateType.Idle
+        );
+    }
+       
 }
