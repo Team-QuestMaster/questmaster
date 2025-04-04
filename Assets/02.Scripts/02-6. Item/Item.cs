7c36089b1883f0301148b558968548206e6372d7
BBTibbers <youngsuk0311@gmail.com>
2025-04-04 13:09:46 +0900

    Feat | 테스트용아이템들구현

--- Assets/02.Scripts/02-6. Item/Item.cs
+++ Assets/02.Scripts/02-6. Item/Item.cs
@@ -8,6 +8,7 @@ public abstract class Item : MonoBehaviour // 아이템 추상 클래스
     [SerializeField]
     private string _description;
     public string Description { get => _description; }
-    public abstract void Use(); //추상함수
+    public abstract void Use(Adventurer adventurer, Quest quest); //퀘스트 수주 시 아이템 사용
+    public abstract void Rollback(Adventurer adventurer, Quest quest); // 퀘스트 종료시 아이템 효과 삭제
 
 }
---
+++ Assets/02.Scripts/02-6. Item/TestItems/TestReducePenalty.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class TestReducePenalty : Item
+{
+    // 보상 증가 및 패널티 감소 아이템은 중복 적용하면 안됩니다.
+    // float에서 int로 형변환을 했기 때문에 역연산으로 복구하지 않고 원본값을 저장합니다.
+    // 순서가 꼬일 수 있는 구조이므로 유의하시기 바랍니다. 
+
+    private int _originalFamePenalty;
+    private int _originalGoldPenalty;
+
+    public override void Use(Adventurer adventurer, Quest quest) // 재구현
+    {
+        Debug.Log($"{quest.name}의 패널티 감소 : {Name}");
+        _originalFamePenalty = quest.QuestData.FamePenalty; // 원본값 저장
+        _originalGoldPenalty = quest.QuestData.GoldPenalty; // 원본값 저장
+        quest.QuestData.FamePenalty = (int)(0.5f * quest.QuestData.FamePenalty); // 패널티 감소
+        quest.QuestData.GoldPenalty = (int)(0.5f * quest.QuestData.GoldPenalty); 
+        ItemManager.Instance.RemoveItem(this);
+    }
+
+    public override void Rollback(Adventurer adventurer, Quest quest) // 재구현
+    {
+        Debug.Log($"{quest.name}의 성공 확률 복구 : {Name}");
+        quest.QuestData.FamePenalty = _originalFamePenalty; // 패널티 복구
+        quest.QuestData.GoldPenalty = _originalGoldPenalty; 
+    }
+}
+
