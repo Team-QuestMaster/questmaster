37e395f3d0cfb951180857a34f49077e62032d1d
BBTibbers <youngsuk0311@gmail.com>
2025-04-04 16:09:47 +0900

    Feat | 아이템 상점 구현 및 아이템클래스 구조 변경

--- Assets/02.Scripts/02-6. Item/Item.cs
+++ Assets/02.Scripts/02-6. Item/Item.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public abstract class Item : MonoBehaviour // 아이템 추상 클래스
+public abstract class Item : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler // 아이템 추상 클래스
 {
     [SerializeField]
     private string _name;
@@ -8,7 +9,19 @@ public abstract class Item : MonoBehaviour // 아이템 추상 클래스
     [SerializeField]
     private string _description;
     public string Description { get => _description; }
+    [SerializeField]
+    private string _prize;
+    public string Prize { get => _prize; }
     public abstract void Use(Adventurer adventurer, Quest quest); //퀘스트 수주 시 아이템 사용
     public abstract void Rollback(Adventurer adventurer, Quest quest); // 퀘스트 종료시 아이템 효과 삭제
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //UIManager.Instance.ShowTooltip(this);
+    }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //UIManager.Instance.HideTooltip();
+    }
 
 }
--- Assets/02.Scripts/02-6. Item/ItemManager.cs
+++ Assets/02.Scripts/02-6. Item/ItemManager.cs
@@ -1,15 +1,20 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using UnityEngine;
 
 public class ItemManager : Singleton <ItemManager>
 {
     [SerializeField]
-    private List<Item> _itemList = new List<Item>(); // 전체 아이템 리스트
-    public List<Item> ItemList { get => _itemList; } 
+    private List<Item> _remainItemList = new List<Item>(); // 미보유 아이템 리스트
+    public List<Item> RemainItemList { get => _remainItemList; } 
 
     private List<Item> _havingItemList = new List<Item>(); // 보유 아이템 리스트
     public List<Item> HavingItemList { get => _havingItemList;}
+
+    private List<Item> _shopingList = new List<Item>(); // 상점 아이템 리스트
+    public List<Item> ShopingList { get => _shopingList; set => _shopingList = value; }
+    private const int SHOP_ITEM_COUNT = 3; // 야시장 물품 수
 
     [SerializeField]
     private int _inventoryMaxCount; // 인벤토리 최대 칸 수, 불변
@@ -24,30 +29,59 @@ public class ItemManager : Singleton <ItemManager>
         base.Awake();
     }
 
-    public void AddItem(Item item) // 보유 아이템에 추가
+    public void BuyItem(Item item) // 상점에서 제거, 보유 아이템에 추가
     {
         if (IsInventoryFull())
         {
             Debug.Log("인벤토리가 가득 찼습니다.");
             return;
         }
-        _havingItemList.Add(item);
+
+        ShopingList.Remove(item); 
+        HavingItemList.Add(item);
     }
 
-    public void RemoveItem(Item item) // 보유 아이템에서 제거
+    public void ItemUsed(Item item) // 보유 아이템에서 제거
     {
         if(ReferenceEquals(item, null))
         {
             Debug.Log("아이템이 NULL입니다.");
             return;
         }
-        if(!_havingItemList.Contains(item))
+        if(!HavingItemList.Contains(item))
         {
             Debug.Log("인벤토리에 해당 아이템이 없습니다.");
             return;
         }
-        _havingItemList.Remove(item);
+        HavingItemList.Remove(item);
     }
+
+    public void SellingItems() // 야시장 물품 3개 추출
+    {                          // List 리턴하는 식으로 구현하면 복사본이 생기는게 찝찝해서 void로 구현함
+        for (int i = 0; i < SHOP_ITEM_COUNT; i++)
+        {
+            if(RemainItemList.Count == 0) // 미보유 아이템 리스트가 비어있으면 종료
+            {
+                Debug.Log("미보유 아이템 리스트가 비었습니다.");
+                return;
+            }
+            Item item = RemainItemList[Random.Range(0, RemainItemList.Count)];
+            ShopingList.Add(item); // 상점 아이템 리스트에 추가
+            RemainItemList.Remove(item); // 미보유 아이템 리스트에서 제거
+        }
+    }
+
+    public void ReturnItems() // 상점에 남은 아이템 미보유아이템에 추가
+    {
+        foreach (Item item in ShopingList)
+        {
+            RemainItemList.Add(item); 
+            ShopingList.Remove(item);
+        }
+    }
+
+
+
 
     public bool IsInventoryFull() // 인벤토리 가득 찼는지 확인
     {
