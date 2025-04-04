1b6941ce4c3ff2e91fb0ac40a6a04188d11cfea2
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-04 10:36:27 +0900

    Docs | Date 폴더 삭제 및 DateManager 스크립트 System 폴더로 이관

--- Assets/02.Scripts/02-9. System/DateManager.cs
+++ Assets/02.Scripts/02-9. System/DateManager.cs
@@ -2,23 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-public class QuestResult
-{
-    private Adventurer _adventurer;
-    public Adventurer Adventurer { get => _adventurer; set => _adventurer = value; }
-    private Quest _quest;
-    public Quest Quest { get => _quest; set => _quest = value; }
-    private bool _isSuccess;
-    public bool IsSuccess { get => _isSuccess; set => _isSuccess = value; }
-    public QuestResult(ref Adventurer adventurer, ref Quest quest, bool isSuccess)
-    {
-        _adventurer = adventurer;
-        _quest = quest;
-        _isSuccess = isSuccess;
-    }
-}
-
 public class DateManager : Singleton<DateManager>
 {
     private const int _maxDate = 30;
