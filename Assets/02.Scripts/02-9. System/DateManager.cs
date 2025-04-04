272359712f4ed692068267f9e74b6c7cafa785de
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-04 13:35:36 +0900

    Feat | DateManager 내 TimePeriod(시간대) 및 날짜, 시간대 변경 로직 구현

--- Assets/02.Scripts/02-9. System/DateManager.cs
+++ Assets/02.Scripts/02-9. System/DateManager.cs
@@ -1,6 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public enum TimePeriod
+{
+    Morning, // 2
+    Afternoon, // 2
+    Evening, // 1
+    Night
+}
+
 
 public class DateManager : Singleton<DateManager>
 {
@@ -12,8 +22,13 @@ public class DateManager : Singleton<DateManager>
         get => _currentDate;
         set => _currentDate = Mathf.Clamp(value, 1, _maxDate);
     }
+    private TimePeriod _currentTimePeriod = TimePeriod.Morning;
+    public TimePeriod CurrentTimePeriod { get => _currentTimePeriod; }
+
     private List<List<QuestResult>> _questResults = new List<List<QuestResult>>(_maxDate + 1);
 
+    public Action OnTimePeriodChanged; 
+    // 오전, 낮, 저녁, 밤과 같이 시간대가 바뀌었을 때, Background 등의 변경을 위한 Action
 
     protected override void Awake()
     {
@@ -23,12 +38,11 @@ public class DateManager : Singleton<DateManager>
             _questResults.Add(new List<QuestResult>());
         }
     }
-
     private void Start()
     {
-        
+        PickManager.Instance.OnVisitedAdventurerCountIncreased += ChangeDate;
+        PickManager.Instance.OnVisitedAdventurerCountIncreased += ChangeTimePeriod;
     }
-
     public void AddQuestResultToList(ref Adventurer adventurer, ref Quest quest, bool isSuccess)
     {
         int endDay = quest.QuestData.Days + _currentDate;
@@ -41,5 +55,34 @@ public class DateManager : Singleton<DateManager>
     public List<QuestResult> GetTodayQuestResults()
     {
         return _questResults[_currentDate];
+    }
+    private void ChangeDate()
+    {
+        if (PickManager.Instance.IsVisitedAdventuererCountMax())
+        {
+            _currentDate++;
+            _currentTimePeriod = TimePeriod.Morning;
+            PickManager.Instance.VisitedAdventurerCount = 0;
+        }
+    }
+    private void ChangeTimePeriod()
+    {
+        int currentVisitedAdventurerCount = PickManager.Instance.VisitedAdventurerCount;
+        if (currentVisitedAdventurerCount <= 2)
+        {
+            _currentTimePeriod = TimePeriod.Morning;
+        }
+        else if (currentVisitedAdventurerCount <= 4)
+        {
+            _currentTimePeriod = TimePeriod.Afternoon;
+        }
+        else
+        {
+            _currentTimePeriod = TimePeriod.Evening;
+        }
+    }
+    private void BeginNightEvent()
+    {
+        _currentTimePeriod = TimePeriod.Night;
     }
 }
