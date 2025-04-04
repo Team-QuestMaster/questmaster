7b223115a56484042dc861b4450b156c3c0300e0
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-04 16:24:40 +0900

    Style | PickManager 내 현재 모험가, 퀘스트 정보 및 방문수 카운트 제거
    해당 변수들을 제거하고, 추후 구현할 MainProcess로 이관해주기로 했다.
    유효한 모험가와 퀘스트를 뽑는 PickManager의 단일 책임을 유지하고, 해당 싱글톤 매니저에 과도한 의존을 피하기 위함이다.

--- Assets/02.Scripts/02-9. System/PickManager.cs
+++ Assets/02.Scripts/02-9. System/PickManager.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -14,46 +13,22 @@ public class PickManager : Singleton<PickManager>
     private Dictionary<string, Quest> _quests = new Dictionary<string, Quest>();
     public Dictionary<string, Quest> Quests { get => _quests; set => _quests = value; }
 
-    private Adventurer _currentAdventurer;
-    public Adventurer CurrentAdventurer { get => _currentAdventurer; }
-    private Quest _currentQuest;
-    public Quest CurrentQuest { get => _currentQuest; }
-
-    private const int _visitedAdventurerCountMax = 5; // 하루에 방문 가능한 최대 모험가의 수
-    private int _visitedAdventurerCount = 0; // 현재 방문한 모험가의 수
-    public int VisitedAdventurerCount
-    {
-        get => _visitedAdventurerCount;
-        set => _visitedAdventurerCount = value;
-    }
-
-    public event Action OnVisitedAdventurerCountIncreased;
-
     protected override void Awake()
     {
         base.Awake();
     }
     public (Adventurer, Quest) Pick()
     {
-        if (_visitedAdventurerCountMax <= _visitedAdventurerCount)
+        Adventurer currentAdventurer = TryPickAdventurer();
+        Quest currentQuest = TryPickQuest();
+        if (ReferenceEquals(currentAdventurer, null) || ReferenceEquals(currentQuest, null))
         {
-            Debug.Log("더 이상 (모험가, 퀘스트) 쌍을 뽑을 수 없습니다.");
-            return (null, null);
-
-        }
-        _currentAdventurer = PickAdventurer();
-        _currentQuest = PickQuest();
-        if (ReferenceEquals(_currentAdventurer, null) || ReferenceEquals(_currentQuest, null))
-        {
-            Debug.Log("현재 뽑아올 수 있는 모험가 또는 퀘스트가 없습니다.");
+            Debug.Log("현재 뽑아올 수 있는 유효한 모험가 또는 퀘스트가 없습니다.");
             return (null, null);
         }
-
-        _visitedAdventurerCount++;
-        OnVisitedAdventurerCountIncreased?.Invoke();
-        return (_currentAdventurer, _currentQuest);
+        return (currentAdventurer, currentQuest);
     }
-    private Adventurer PickAdventurer()
+    private Adventurer TryPickAdventurer()
     {
         if (_adventurers.Count == 0)
         {
@@ -73,12 +48,13 @@ public class PickManager : Singleton<PickManager>
         }
         return null;
     }
-    private Quest PickQuest()
+    private Quest TryPickQuest()
     {
         if (_quests.Count == 0)
         {
             return null;
         }
+
         int count = 0;
         while (count < _quests.Count)
         {
@@ -91,9 +67,5 @@ public class PickManager : Singleton<PickManager>
             count++;
         }
         return null;
-    }
-    public bool IsVisitedAdventuererCountMax()
-    {
-        return _visitedAdventurerCount == _visitedAdventurerCountMax;
     }
 }
