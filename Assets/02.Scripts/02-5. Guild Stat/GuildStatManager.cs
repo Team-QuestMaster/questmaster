391465f9605c4655a258ae86b92bfa11739d650b
OhSiHyeon <osh000308@gmail.com>
2025-04-04 13:00:09 +0900

    Style | 유니티 이벤트 컨벤션 준수

--- Assets/02.Scripts/02-5. Guild Stat/GuildStatManager.cs
+++ Assets/02.Scripts/02-5. Guild Stat/GuildStatManager.cs
@@ -8,11 +8,6 @@ public class GuildStatManager : Singleton<GuildStatManager>
     // 명성은 0~올릴 수 있는 만큼
     // 돈은 세금으로 음수 가능, 아이템 구매 시 음수로 구매 불가~올릴 수 있는 만큼
 
-    protected override void Awake()
-    {
-        base.Awake();
-    }
-
     private int _fame;
     public int Fame
     {
@@ -37,6 +32,11 @@ public class GuildStatManager : Singleton<GuildStatManager>
 
     public event Action OnFameChanged;
     public event Action OnGoldChanged;
+    
+    protected override void Awake()
+    {
+        base.Awake();
+    }
     
     /// <summary>
     /// 기능: 돈이 사용이 가능한지 확인 후 사용
