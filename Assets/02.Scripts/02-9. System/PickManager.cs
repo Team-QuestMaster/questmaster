a420b4fb4d27d6c8305bc2efb8c5d081d1827f10
Mingyu0626 <mg010626@naver.com>
2025-04-05 15:05:06 +0900

    Hotfix | 전체 모험가 퀘스트 저장 자료구조 리스트로 변경
    Dictionary는 직렬화가 불가능하여 유니티 에디터 인스펙터 상에서 조회가 안되는 점을 고려하여, 전체 모험가와 퀘스트를 저장하는 자료구조를 List로 변경하였습니다.

--- Assets/02.Scripts/02-9. System/PickManager.cs
+++ Assets/02.Scripts/02-9. System/PickManager.cs
@@ -6,13 +6,11 @@ using UnityEngine;
 public class PickManager : Singleton<PickManager>
 {
     [SerializeField]
-    private Dictionary<string, Adventurer> _adventurers = new Dictionary<string, Adventurer>();
-    public Dictionary<string, Adventurer> Adventurers { get => _adventurers; set => _adventurers = value; }
-
+    private List<Adventurer> _adventurers = new List<Adventurer>();
+    public List<Adventurer> Adventurers { get => _adventurers; set => _adventurers = value; }
     [SerializeField]
-    private Dictionary<string, Quest> _quests = new Dictionary<string, Quest>();
-    public Dictionary<string, Quest> Quests { get => _quests; set => _quests = value; }
-
+    private List<Quest> _quests = new List<Quest>();
+    public List<Quest> Quests { get => _quests; set => _quests = value; }
     protected override void Awake()
     {
         base.Awake();
@@ -39,7 +37,7 @@ public class PickManager : Singleton<PickManager>
         while (count < _adventurers.Count)
         {
             int randomIndex = UnityEngine.Random.Range(0, _adventurers.Count);
-            Adventurer adventurer = _adventurers.ElementAt(randomIndex).Value;
+            Adventurer adventurer = _adventurers[randomIndex];
             if (adventurer.AdventurerData.AdventurerState == AdventurerStateType.Idle)
             {
                 return adventurer;
@@ -48,6 +46,7 @@ public class PickManager : Singleton<PickManager>
         }
         return null;
     }
+
     private Quest PickQuest()
     {
         if (_quests.Count == 0)
@@ -59,7 +58,7 @@ public class PickManager : Singleton<PickManager>
         while (count < _quests.Count)
         {
             int randomIndex = UnityEngine.Random.Range(0, _quests.Count);
-            Quest quest = _quests.ElementAt(randomIndex).Value;
+            Quest quest = _quests[randomIndex];
             if (!quest.QuestData.IsQuesting)
             {
                 return quest;
