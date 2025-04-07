13fdf5396f02fb3abbbabfa0ee2c3ad71656088f
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-07 16:14:00 +0900

    Add | StageShowMaanger 내 ShowQuest 참조 추가

--- Assets/02.Scripts/02-8. UI/Show/StageShowManager.cs
+++ Assets/02.Scripts/02-8. UI/Show/StageShowManager.cs
@@ -4,6 +4,7 @@ public class StageShowManager : Singleton<StageShowManager>
 {
     public ShowResult ShowResult;
     public ShowCharacter ShowCharacter;
+    public ShowQuest ShowQuest;
     public MiniCharacterUI MiniCharacter;
     
 }
