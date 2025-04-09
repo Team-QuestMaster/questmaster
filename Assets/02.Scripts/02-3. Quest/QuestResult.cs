d93de1d52b713b6b99790704f53e4953b671577f
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-09 09:59:37 +0900

    Fix | QuestResult 클래스 모험가 데이터 및 퀘스트 데이터를 담도록 수정

--- Assets/02.Scripts/02-3. Quest/QuestResult.cs
+++ Assets/02.Scripts/02-3. Quest/QuestResult.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 
 public class QuestResult
 {
-    private Adventurer _adventurer;
-    public Adventurer Adventurer { get => _adventurer; set => _adventurer = value; }
-    private Quest _quest;
-    public Quest Quest { get => _quest; set => _quest = value; }
+    private AdventurerData _adventurerData;
+    public AdventurerData AdventurerData { get => _adventurerData; set => _adventurerData = value; }
+    private QuestData _questData;
+    public QuestData QuestData { get => _questData; set => _questData = value; }
     private bool _isSuccess;
     public bool IsSuccess { get => _isSuccess; set => _isSuccess = value; }
 
     private float _probability;
     public float Probability { get => _probability; set => _probability = value; }
 
-    public QuestResult(ref Adventurer adventurer, ref Quest quest, bool isSuccess, float probability)
+    public QuestResult(AdventurerData adventurerData, QuestData questData, bool isSuccess, float probability)
     {
-        _adventurer = adventurer;
-        _quest = quest;
+        _adventurerData = adventurerData;
+        _questData = questData;
         _isSuccess = isSuccess;
         _probability = probability;
     }
