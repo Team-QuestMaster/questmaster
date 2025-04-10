4347b01a3f9f76e5d4d106fa7123476b30d271d7
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-10 18:14:40 +0900

    Fix | MinorAdventureSO 구조 변경

--- Assets/02.Scripts/02-2. Adventurer/Data/MinorASO.cs
+++ Assets/02.Scripts/02-2. Adventurer/Data/MinorASO.cs
@@ -2,20 +2,12 @@ using NUnit.Framework;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[System.Serializable]
-public class AdventurerSpritePair
-{
-    public Sprite SDSprite;
-    public Sprite LDSprite;
-}
-
 [CreateAssetMenu(fileName = "MinorASO", menuName = "ScriptableObject/MinorASO")]
 public class MinorASO : AdventurerSO
-{
-    // 아래 존재하는 리스트들은, 모두 마이너 모험가가 
+{ 
     public List<string> AdventurerNameList = new List<string>();  // 이름
     public List<string> AdventurerClassList = new List<string>();  // 직업
-    public List<string> AdventurerTitleList = new List<string>();  // 칭호 리스트
-    public List<AdventurerSpritePair> AdventurerSpritePairList = new List<AdventurerSpritePair>();
+    public List<string> AdventurerTitleList = new List<string>();  // 칭호
+    public Sprite AdventurerSpriteSD; // 모든 마이너 모험가가 공유하는 SD 스프라이트
+    public List<Sprite> AdventurerSpriteLDList = new List<Sprite>(); // LD 스프라이트 
 }
