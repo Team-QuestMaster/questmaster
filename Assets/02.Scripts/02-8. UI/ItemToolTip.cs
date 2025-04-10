b1aaa6bdcb58bf2b4e381dd426405875b71671e7
OhSiHyeon <osh000308@gmail.com>
2025-04-10 17:13:29 +0900

    Fix | 아이템 툴팁 수정

--- Assets/02.Scripts/02-8. UI/ItemToolTip.cs
+++ Assets/02.Scripts/02-8. UI/ItemToolTip.cs
@@ -2,14 +2,27 @@ using System;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-[RequireComponent(typeof(Item))]
 public class ItemToolTip : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDragHandler, IEndDragHandler
 {
     private Item _item;
 
     private void Awake()
     {
-        _item = GetComponent<Item>();
+        if (!TryGetComponent(out _item))
+        {
+            // 스몰인 경우 빅에서 찾아서 item정보를 가져옴
+            if (TryGetComponent(out DraggingObjectSwap objectSwap))
+            {
+                if (!objectSwap.SwapTargetObject.TryGetComponent(out _item))
+                {
+                    Debug.LogError($"Item 정보를 가져올 수 없습니다.");
+                }
+            }
+            else
+            {
+                Debug.LogError($"빅 오브젝트 정보를 가져올 수 없습니다. 스왑을 확인해 주세요");
+            }
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
