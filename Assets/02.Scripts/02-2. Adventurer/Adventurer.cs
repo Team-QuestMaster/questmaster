4206005ce8ba5386ef8aaa4ba6020950434e878b
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-10 10:51:29 +0900

    Feat | 모험가 현재 스탯합(전투력)에 따른 티어 이미지 변경 구현

--- Assets/02.Scripts/02-2. Adventurer/Adventurer.cs
+++ Assets/02.Scripts/02-2. Adventurer/Adventurer.cs
@@ -67,8 +67,7 @@ public class Adventurer : MonoBehaviour
             = minorAdventurerSO.AdventurerTitleList[Random.Range(0, minorAdventurerSO.AdventurerTitleList.Count)];
         DialogSet dialogSet
             = minorAdventurerSO.DialogList[Random.Range(0, minorAdventurerSO.DialogList.Count)];
-        AdventurerTierType adventurerTier
-            = (AdventurerTierType)Random.Range((int)AdventurerTierType.A, (int)AdventurerTierType.D + 1);
+        AdventurerTierType adventurerTier = AdventurerTierType.A;
         AdventurerSpritePair spritesPair
             = minorAdventurerSO.AdventurerSpritePairList[Random.Range(0, minorAdventurerSO.AdventurerSpritePairList.Count)];
 
@@ -112,12 +111,34 @@ public class Adventurer : MonoBehaviour
         _adventurerData.AdventurerClass = minorAdventurerSO.AdventurerClassList[Random.Range(0, minorAdventurerSO.AdventurerClassList.Count)];
         _adventurerData.AdventurerTitle = minorAdventurerSO.AdventurerTitleList[Random.Range(0, minorAdventurerSO.AdventurerTitleList.Count)];
         _adventurerData.DialogSet = minorAdventurerSO.DialogList[Random.Range(0, minorAdventurerSO.DialogList.Count)];
-        _adventurerData.AdventurerTier = (AdventurerTierType)Random.Range((int)AdventurerTierType.A, (int)AdventurerTierType.D + 1);
         AdventurerSpritePair spritesPair
             = minorAdventurerSO.AdventurerSpritePairList[Random.Range(0, minorAdventurerSO.AdventurerSpritePairList.Count)];
         _adventurerData.SpriteSD = spritesPair.SDSprite;
         _adventurerData.SpriteLD = spritesPair.LDSprite;
 
         _minerStatHandler.SetRandomStat(this, DateManager.Instance.CurrentDate);
+        SetAdventurerTierOnPower();
+    }
+    private void SetAdventurerTierOnPower()
+    {
+        int currentPower = _adventurerData.CurrentSTR + _adventurerData.CurrentMAG + _adventurerData.CurrentINS + _adventurerData.CurrentDEX;
+        int tierIndex = 0;
+        if (currentPower < 3200)
+        {
+            tierIndex = 0;
+        }
+        else if (3200 <= currentPower && currentPower < 6000)
+        {
+            tierIndex = 1;
+        }
+        else if (6000 <= currentPower && currentPower < 8800)
+        {
+            tierIndex = 2;
+        }
+        else
+        {
+            tierIndex = 3;
+        }
+        _adventurerData.AdventurerTier = (AdventurerTierType)tierIndex;
     }
 }
