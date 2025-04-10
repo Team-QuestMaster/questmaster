1fa0a671f485dfa9cd770103c34c112b76af6a84
AByeong <106604214+AByeong@users.noreply.github.com>
2025-04-10 17:16:35 +0900

    Feat | 엔딩씬
    엔딩씬 만들었습니다.

--- Assets/02.Scripts/02-1. Core/SceneMover.cs
+++ Assets/02.Scripts/02-1. Core/SceneMover.cs
@@ -1,8 +1,23 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneMover : Singleton<SceneMover>
 {
+
+    [SerializeField] private AudioSource _bgm;
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private void Start()
+    {
+        _bgm = GetComponent<AudioSource>();
+        _bgm.Play();
+    }
 
     public void MoveScene(string sceneName)
     {
