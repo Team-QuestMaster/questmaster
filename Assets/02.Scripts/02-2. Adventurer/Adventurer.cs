0059f24678f55b218fcbed12f60599885fab467f
Mingyu Choi <88257032+Mingyu0626@users.noreply.github.com>
2025-04-10 09:14:43 +0900

    Refactor | Adventurer 클래스 내 모험가 데이터 변경 로직 Init과 Set으로 분리

--- Assets/02.Scripts/02-2. Adventurer/Adventurer.cs
+++ Assets/02.Scripts/02-2. Adventurer/Adventurer.cs
@@ -15,31 +15,31 @@ public class Adventurer : MonoBehaviour
 
     private void Awake()
     {
-        SetAdventurerData(1);
+        InitAdventurerData();
         gameObject.SetActive(false);
     }
     private void OnEnable()
     {
-        SetAdventurerData(DateManager.Instance.CurrentDate);
+        SetAdventurerData();
     }
     private void OnDisable()
     {
 
     }
-    private void SetAdventurerData(int currentDate)
+    private void InitAdventurerData()
     {
         if (_adventurerSO.AdventurerType == AdventurerType.Major || _adventurerSO.AdventurerType == AdventurerType.Dealer)
         {
             MajorASO majorAdventurerSO = (MajorASO)_adventurerSO;
-            SetMajorTypeData(majorAdventurerSO);
+            InitMajorTypeData(majorAdventurerSO);
         }
         else
         {
             MinorASO minorAdventurerSO = (MinorASO)_adventurerSO;
-            SetMinorTypeData(minorAdventurerSO, currentDate);
+            InitMinorTypeData(minorAdventurerSO);
         }
     }
-    private void SetMajorTypeData(MajorASO majorAdventurerSO)
+    private void InitMajorTypeData(MajorASO majorAdventurerSO)
     {
         _adventurerData = new AdventurerData(
             majorAdventurerSO.AdventurerType,
@@ -57,7 +57,7 @@ public class Adventurer : MonoBehaviour
             majorAdventurerSO.SpriteLD
         );
     }
-    private void SetMinorTypeData(MinorASO minorAdventurerSO, int currentDate)
+    private void InitMinorTypeData(MinorASO minorAdventurerSO)
     {
         string adventurerName 
             = minorAdventurerSO.AdventurerNameList[Random.Range(0, minorAdventurerSO.AdventurerNameList.Count)];
@@ -78,15 +78,46 @@ public class Adventurer : MonoBehaviour
             adventurerClass,
             adventurerTitle,
             adventurerTier,
-            minorAdventurerSO.OriginalSTR,
-            minorAdventurerSO.OriginalMAG,
-            minorAdventurerSO.OriginalINS,
-            minorAdventurerSO.OriginalDEX,
+            _adventurerData.CurrentSTR,
+            _adventurerData.CurrentMAG,
+            _adventurerData.CurrentINS,
+            _adventurerData.CurrentDEX,
             AdventurerStateType.Idle,
             dialogSet,
             spritesPair.SDSprite,
             spritesPair.LDSprite
         );
-        _minerStatHandler.SetRandomStat(this, currentDate);
+    }
+    private void SetAdventurerData()
+    {
+        if (_adventurerSO.AdventurerType == AdventurerType.Major || _adventurerSO.AdventurerType == AdventurerType.Dealer)
+        {
+            MajorASO majorAdventurerSO = (MajorASO)_adventurerSO;
+            SetMajorTypeData(majorAdventurerSO);
+        }
+        else
+        {
+            MinorASO minorAdventurerSO = (MinorASO)_adventurerSO;
+            SetMinorTypeData(minorAdventurerSO);
+        }
+    }
+    private void SetMajorTypeData(MajorASO majorAdventurerSO)
+    {
+
+    }
+    private void SetMinorTypeData(MinorASO minorAdventurerSO)
+    {
+        _adventurerData.AdventurerType = AdventurerType.Minor;
+        _adventurerData.AdventurerName = minorAdventurerSO.AdventurerNameList[Random.Range(0, minorAdventurerSO.AdventurerNameList.Count)];
+        _adventurerData.AdventurerClass = minorAdventurerSO.AdventurerClassList[Random.Range(0, minorAdventurerSO.AdventurerClassList.Count)];
+        _adventurerData.AdventurerTitle = minorAdventurerSO.AdventurerTitleList[Random.Range(0, minorAdventurerSO.AdventurerTitleList.Count)];
+        _adventurerData.DialogSet = minorAdventurerSO.DialogList[Random.Range(0, minorAdventurerSO.DialogList.Count)];
+        _adventurerData.AdventurerTier = (AdventurerTierType)Random.Range((int)AdventurerTierType.A, (int)AdventurerTierType.D + 1);
+        AdventurerSpritePair spritesPair
+            = minorAdventurerSO.AdventurerSpritePairList[Random.Range(0, minorAdventurerSO.AdventurerSpritePairList.Count)];
+        _adventurerData.SpriteSD = spritesPair.SDSprite;
+        _adventurerData.SpriteLD = spritesPair.LDSprite;
+
+        _minerStatHandler.SetRandomStat(this, DateManager.Instance.CurrentDate);
     }
 }
