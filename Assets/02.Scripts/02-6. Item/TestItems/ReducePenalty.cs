ec50e3dd05e62763ca38359a5ac02bedde9abdc6
BBTibbers <youngsuk0311@gmail.com>
2025-04-11 12:53:02 +0900

    Style | 아이템 프리팹 이름 적용 및 보상증가 통합

--- Assets/02.Scripts/02-6. Item/TestItems/ReducePenalty.cs
+++ Assets/02.Scripts/02-6. Item/TestItems/ReducePenalty.cs
@@ -6,9 +6,8 @@ public class ReducePenalty : QuestItem
     private float _penaltyRate = 0.5f; // 패널티 감소 비율
     public override float QuestUse(Quest quest) // 재구현
     {
-        Debug.Log($"{quest.name}의 패널티 감소");
-        quest.QuestData.FamePenalty = (int)(0.5f * quest.QuestData.FamePenalty); // 패널티 감소
-        quest.QuestData.GoldPenalty = (int)(0.5f * quest.QuestData.GoldPenalty);
+        quest.QuestData.FamePenalty = (int)(_penaltyRate * quest.QuestData.FamePenalty); // 패널티 감소
+        quest.QuestData.GoldPenalty = (int)(_penaltyRate * quest.QuestData.GoldPenalty);
         return 0f;
     }
 }
