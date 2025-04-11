6a030725b555e27d0d139a854d56c63a7b8a0f4a
OhSiHyeon <osh000308@gmail.com>
2025-04-11 15:54:32 +0900

    ADD | 스탬프 터치 사운드

--- Assets/02.Scripts/02-1. Core/EventAudioPlayer.cs
+++ Assets/02.Scripts/02-1. Core/EventAudioPlayer.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class EventAudioPlayer : MonoBehaviour, IPointerClickHandler
+public class EventAudioPlayer : MonoBehaviour, IPointerClickHandler, IPointerDownHandler
 {
     // 여러 이벤트 필요에 따라 추가해서 사용 예정
     [SerializeField]
     private AudioClip _onClickAudioClip;
+    
+    [SerializeField]
+    private AudioClip _onDownAudioClip;
     public void OnPointerClick(PointerEventData eventData)
     {
         if (ReferenceEquals(_onClickAudioClip, null))
@@ -14,5 +17,15 @@ public class EventAudioPlayer : MonoBehaviour, IPointerClickHandler
             return;
         }
         AudioManager.Instance.PlaySFX(_onClickAudioClip);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (ReferenceEquals(_onDownAudioClip, null))
+        {
+            // 오디오가 없으면 재생 안함
+            return;
+        }
+        AudioManager.Instance.PlaySFX(_onDownAudioClip);
     }
 }
