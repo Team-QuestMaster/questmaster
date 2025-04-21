096d07dc33d14e0ccf5dbe247caa3c2d640e7c98
AByeong <106604214+AByeong@users.noreply.github.com>
2025-04-21 11:12:37 +0900

    Refactor | 스탬프 리포트, 페이드 인 아웃
    - 페이드 인 아웃 기능을 새로 만들었습니다.
    - 스탬프 리포트 UI를 구성했습니다.

---
+++ Assets/02.Scripts/02-8. UI/Refactoring/UI_StampedReport.cs
@@ -0,0 +1,78 @@
+using DG.Tweening;
+using TMPro;
+using UnityEngine;
+
+public class UI_StampedReport : MonoBehaviour
+{
+    [SerializeField] 
+    private string _adventurerName;
+    public string AdventurerName {get => _adventurerName; set => _adventurerName = value; }
+    [SerializeField] 
+    private string _questName;
+    public string QuestName { get => _questName; set => _questName = value; }
+    [SerializeField]
+    private float _probability;
+    public float Probability{ get => _probability; set => _probability = value; }
+    [SerializeField] 
+    private int _needTime;
+    public int NeedTime{ get => _needTime; set => _needTime = value; }
+    [SerializeField] 
+    private int _reward;
+    public int Reward{ get => _reward; set => _reward = value; }
+    [SerializeField] 
+    private int _penalty;
+    public int Penalty{ get => _penalty; set => _penalty = value; }
+    [SerializeField] 
+    private bool _approve;
+    public bool Approve { get => _approve; set => _approve = value; }
+    [Header("Hierarchy")]
+    [SerializeField]
+    private TextMeshProUGUI _questNameText;
+    [SerializeField] 
+    private TextMeshProUGUI _adventurerNameText;
+    [SerializeField] 
+    private TextMeshProUGUI _rewardText;
+    [SerializeField]
+    private TextMeshProUGUI _penaltyText;
+    [SerializeField]
+    private TextMeshProUGUI _needTimeText;
+    [SerializeField]
+    private TextMeshProUGUI _probabilityText;
+    [SerializeField]
+    private TextMeshProUGUI _approveOrNotText;
+        
+    [SerializeField]
+    private UI_FadeInOut _fadeInOutImage;
+        
+    public void SetStampedReport(QuestData questData, AdventurerData adventurerData)
+    {
+        
+        _questNameText.text = questData.QuestName;
+        _adventurerNameText.text = adventurerData.AdventurerName;
+        _rewardText.text = questData.GoldReward.ToString();
+        _penaltyText.text = questData.GoldPenalty.ToString();
+        _needTimeText.text = questData.Days.ToString();
+        //probability는 Calculator가 어떻게 동작하는가에 따라 바뀔 예정
+        _probabilityText.text = "30%";
+        _approveOrNotText.text= _approve? "<color=#99A136>승낙함</color>" : "<color=#C4402E>거절됨</color>";
+    }
+
+    public void ShowStampedReport()
+    {
+        
+        //AudioManager.Instance.PlaySFX(_onResultAudioClip);
+        _fadeInOutImage.FadeIn(0.1f);
+        this.transform.DOLocalMove(new Vector3(0, 0, 0), 0.5f);
+        
+    }
+
+    public void HideStampedReport()
+    {
+        _fadeInOutImage.FadeOut(0.1f);
+        this.transform.DOLocalMove(new Vector3(-1470f,0, 0), 0.5f).OnComplete(() =>
+        {
+            //손님 퇴장
+        });
+    }
+    
+}
